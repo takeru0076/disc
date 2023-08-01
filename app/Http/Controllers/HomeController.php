2e4328a0910e02685cba7b6119b19c0827fc2e74
takeru0076 <takeru200276@icloud.com>
2023-08-02 07:48:17 +0900

    追加:whiteboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,4 +11,9 @@ class HomeController extends Controller
     {
         return Inertia::render("Front/Home");
     }
+    
+    public function whiteboard()
+    {
+        return Inertia::render("Front/Whiteboard");
+    }
 }
--- resources/js/Pages/Front/Home.jsx
+++ resources/js/Pages/Front/Home.jsx
@@ -13,13 +13,13 @@ const Home = (props) => {
                     Home
                 </h2>
             }>
-            
             <div className="p-12">
                 <div class="flex flex-col flex items-center">
                     <Link href="/home/strategy" class="text-white item-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-xl mb-4">Strategy</Link>
                     <Link href="/home/rule" class="text-white item-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-xl mb-4">Rule</Link>
                     <Link href="/home/other" class="text-white item-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-xl mb-4">Other</Link>
                     <Link href="/home/kind" class="text-white item-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-xl mb-4">Kind</Link>
+                    <Link href="/home/whiteboard" class="text-white item-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-xl mb-4">WhiteBoard</Link>
                 </div>
             </div>
             
