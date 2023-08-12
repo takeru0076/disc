fe24a52833e3c63e5aceffa005e0809eb3c22dc9
takeru0076 <takeru200276@icloud.com>
2023-08-12 15:27:43 +0900

    修正

--- resources/js/Pages/Front/Home.jsx
+++ resources/js/Pages/Front/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-//import { useNavigate } from "react-router-dom";
-//import { BrowserRouter, Route, Routes } from 'react-router-dom';　//追加したところ
 import { Link , Head } from '@inertiajs/react'; //追加したところ
 import '/resources/css/app.css';
 
